ac739e8b05958c8fea982370ada61c3a470ede66
Ricky Lagerkvist <Ricky Lagerkvist>
2016-01-25 20:18:30 +0100

    Initial commit

---
+++ app/src/main/java/se/rickylagerkvist/golfscorecard/Hole.java
@@ -0,0 +1,30 @@
+package se.rickylagerkvist.golfscorecard;
+
+/**
+ * Created by Ricky on 2016-01-25.
+ */
+public class Hole {
+    private String mLabel;
+    private int mStrokeCount;
+
+    public Hole (String label, int strokeCount){
+        mLabel = label;
+        mStrokeCount = strokeCount;
+    }
+
+    public String getLabel() {
+        return mLabel;
+    }
+
+    public void setLabel(String label) {
+        mLabel = label;
+    }
+
+    public int getStrokeCount() {
+        return mStrokeCount;
+    }
+
+    public void setStrokeCount(int strokeCount) {
+        mStrokeCount = strokeCount;
+    }
+}
